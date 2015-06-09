07cf577c8a77e6771c1aba2b10234ca79936be59
manu7772 <manu7772@gmail.com>
2015-06-09 14:14:01 +0200

    2ème partie mise à jour

---
+++ src/AcmeGroup/LaboBundle/Entity/unite.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace AcmeGroup\LaboBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Doctrine\Common\Collections\ArrayCollection;
+// slug/Tree
+use Gedmo\Mapping\Annotation as Gedmo;
+use AcmeGroup\LaboBundle\Entity\baseUnite;
+
+use \Exception;
+use \DateTime;
+
+/**
+ * unite
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="unite")
+ * @ORM\Entity(repositoryClass="AcmeGroup\LaboBundle\Entity\uniteRepository")
+ * @UniqueEntity(fields={"nom", "version", "statut"}, message="Cette unité existe déjà")
+ */
+class unite extends baseUnite {
+
+
+	public function __construct() {
+		parent::__construct();
+	}
+
+// DEBUT --------------------- à inclure dans toutes les entités ------------------------
+
+	/**
+	 * Renvoie true si l'entité est valide
+	 * @Assert\True(message = "Cette unité n'est pas valide.")
+	 * @return boolean
+	 */
+	public function isValid() {
+		$valid = true;
+		$valid = parent::isValid();
+		if($valid === true) {
+			// opérations pour cette entité
+			// …
+		}
+		return $valid;
+	}
+
+	/**
+	 * Complète les données avant enregistrement
+	 * @ORM\PreUpdate
+	 * @ORM\PrePersist
+	 * @return boolean
+	 */
+	public function verify() {
+		$verif = true;
+		$verif = parent::verify();
+		if($verif === true) {
+			// opérations pour cette entité
+			// …
+		}
+		return $verif;
+	}
+
+// FIN --------------------- à inclure dans toutes les entités ------------------------
+
+
+}
